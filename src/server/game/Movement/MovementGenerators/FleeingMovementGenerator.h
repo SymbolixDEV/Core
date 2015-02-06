22325ba5d95fd10c724ec18828e2c2b579f8f0f0
Symbolixdev <shabi_bjk@abv.bg>
2015-02-06 02:09:01 +0200

    Revert : Temporal Bug

--- src/server/game/Movement/MovementGenerators/FleeingMovementGenerator.h
+++ src/server/game/Movement/MovementGenerators/FleeingMovementGenerator.h
@@ -25,7 +25,7 @@ template<class T>
 class FleeingMovementGenerator : public MovementGeneratorMedium< T, FleeingMovementGenerator<T> >
 {
     public:
-        FleeingMovementGenerator(uint64 fright) : i_frightGUID(fright), i_nextCheckTime(0) {}
+        FleeingMovementGenerator(uint64 fright) : i_frightGUID(fright), i_nextCheckTime(0) { }
 
         void DoInitialize(T*);
         void DoFinalize(T*);
@@ -35,21 +35,9 @@ class FleeingMovementGenerator : public MovementGeneratorMedium< T, FleeingMovem
         MovementGeneratorType GetMovementGeneratorType() { return FLEEING_MOTION_TYPE; }
 
     private:
-        void _setTargetLocation(T* owner);
-        bool _getPoint(T* owner, float &x, float &y, float &z);
-        bool _setMoveData(T* owner);
-        void _Init(T*);
- 
-        bool is_water_ok   :1;
-        bool is_land_ok    :1;
-        bool i_only_forward:1;
- 
-        float i_caster_x;
-        float i_caster_y;
-        float i_caster_z;
-        float i_last_distance_from_caster;
-        float i_to_distance_from_caster;
-        float i_cur_angle;
+        void _setTargetLocation(T*);
+        void _getPoint(T*, float &x, float &y, float &z);
+
         uint64 i_frightGUID;
         TimeTracker i_nextCheckTime;
 };
@@ -59,11 +47,11 @@ class TimedFleeingMovementGenerator : public FleeingMovementGenerator<Creature>
     public:
         TimedFleeingMovementGenerator(uint64 fright, uint32 time) :
             FleeingMovementGenerator<Creature>(fright),
-            i_totalFleeTime(time) {}
+            i_totalFleeTime(time) { }
 
         MovementGeneratorType GetMovementGeneratorType() { return TIMED_FLEEING_MOTION_TYPE; }
-        bool DoUpdate(Unit*, uint32);
-        void DoFinalize(Unit*);
+        bool Update(Unit*, uint32);
+        void Finalize(Unit*);
 
     private:
         TimeTracker i_totalFleeTime;
